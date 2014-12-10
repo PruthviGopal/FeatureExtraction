0005734a5332c7306e080a627672281609c5cb3d
ivancheesecake <ivanhescamos@gmail.com>
2014-12-10 10:43:40 +0800

    Added a bit of documentation
    :))

--- featureExtractionV1.py
+++ featureExtractionV1.py
@@ -1,3 +1,8 @@
+#
+#	Filename: featureExtractionV1.py
+#	Description: An ArcGIS script for the automated detection of man-made features in LiDAR datasets
+#	Written by: Phil-LiDAR 1 UPLB	
+
 import arcpy
 import numpy
 from scipy import ndimage
@@ -13,11 +18,10 @@ try:
 
 	arcpy.AddMessage("Retrieving inputs...")
 
-	# inDSMRas = arcpy.Raster(inDSM) 
-	# inDTMRas = arcpy.Raster(inDTM)
+#	Retrieve the needed data for raster output
+#	(starting x,y coordinates, cell size, no data value and spatial reference)
 
 	desc = arcpy.Describe(inDSM)
-
 	xmin = desc.extent.XMin
 	ymin = desc.extent.YMin
 	cellwidth = desc.meanCellWidth
@@ -25,18 +29,21 @@ try:
 	nodata = desc.noDataValue
 	sr = desc.spatialReference
 
+#	Calculate the difference raster
+
 	arcpy.AddMessage("Calculating height raster...")
 
-	#fix DTM and DTM first
+	# Convert rasters to numpy arrays
 	dsmArr = arcpy.RasterToNumPyArray(inDSM)
 	dtmArr = arcpy.RasterToNumPyArray(inDTM)
 	
-
+	# Prepare for calculations
 	numrows = len(dsmArr)
 	numcols = len(dsmArr[0])
-
 	heightArr = copy.deepcopy(dsmArr)
 
+	# Get the difference of every pixel from the DSM and DTM
+	# Make sure there are no negative values in the resultant numpy array
 	for y in range(numrows):
 		for x in range(numcols):
 			if dsmArr[y][x] < 0 or dtmArr[y][x]  <0:
@@ -44,13 +51,9 @@ try:
 			else:
 				heightArr[y][x] = dsmArr[y][x] - dtmArr[y][x]
 
-	# heightArr = dsmArr-dtmArr
-	# heightRas = arcpy.Raster(inDSM) - arcpy.Raster(inDTM)
-	# heightArr = arcpy.RasterToNumPyArray(heightRas)
 	heightArr[heightArr<0] = 0
 
-	# arcpy.AddMessage(str(heightArr.max()))
-	
+#	Remove presumed vegetation pixels	
 	arcpy.AddMessage("Removing (obvious) vegetation...")
 
 	stdArr = copy.deepcopy(heightArr)
@@ -64,6 +67,7 @@ try:
 
 	arcpy.AddMessage("Removing (obvious) vegetation...Processing Grids")
 
+	# Per 2x2 grid, calculate for the standard deviation
 	while y< numrows:
 		while x<numcols:
 			if y<numrows and x < numcols and heightArr[y][x] > 0: 
@@ -96,60 +100,63 @@ try:
 
 	arcpy.AddMessage("Removing (obvious) vegetation...Filtering...")
 
+	# Apply a median filter blur the standard deviation array
 	stdArrFiltered = ndimage.filters.median_filter(stdArr,(5,5))
 	stdevThresh = stdArrFiltered.max() * 0.125
-	arcpy.AddMessage(str(stdevThresh))
-
-	arcpy.AddMessage(str(stdArrFiltered))
-	arcpy.AddMessage(stdevThresh)
+	
+	# Remove pixels from the height raster if they belong to a grid cell with a high stdev	
 	for y in range (0,numrows):
 		for x in range(0,numcols):
 			if stdArrFiltered[y][x] > stdevThresh:
-				# arcpy.AddMessage("Hello. :)")
 				noVegArr[y][x] = 0
 
-	# tempRas = arcpy.NumPyArrayToRaster(noVegArr)
-	# tempRas.save("C:/FeatureExtractionV1/noveg.tif")
-
+#	Perform binarization
 	arcpy.AddMessage("Height Thresholding...")
 	thresh = noVegArr > 2
-
+	
+	# Perform morphological operations for binary image cleaning
 	clean = ndimage.morphology.binary_erosion(thresh)
-	# clean = ndimage.morphology.binary_dilation(clean)
 	clean = ndimage.morphology.binary_fill_holes(clean)
 
 	heightCleaned = clean * 1
 
+#	Label connected components using an integer id
 	arcpy.AddMessage("Connected Component Labeling...")
 	labeled, numLabels = ndimage.label(heightCleaned)
 	outArr = copy.deepcopy(labeled)
 
-
+#	Remove connected components that are smaller than the user-defined threshold
 	arcpy.AddMessage("Connected Component Cleaning...")
-	
+
+	# Create a histogram with every label id as bins
 	histogram = [0]*(labeled.max()+1)
 	droplist = []
 
 	arcpy.AddMessage("Connected Component Cleaning...Calculating Histogram...")	
+	
+	# Increment bins
 	for y in range(0,len(labeled)):
 		for x in range(0, len(labeled[0])):
 			histogram[labeled[y][x]] +=1
 
+	# If the number of pixels is less than the threshold, add the label id to the droplist
 	arcpy.AddMessage("Connected Component Cleaning...Populating droplist...")	
 	for i in range(0, len(histogram)):
 		if histogram[i] < sizeThresh:	
 			droplist.append(i)
 	
+	# Remove connected components with labels inside the droplist 
 	arcpy.AddMessage("Connected Component Cleaning...Dropping connected components...")			
 	for i in range(0, len(droplist)):    
 		outArr[outArr == droplist[i]] = 0
 
+	# Save the binary image	
 	arcpy.AddMessage("Saving output raster...")			
 	outBinaryRas = arcpy.NumPyArrayToRaster(outArr, arcpy.Point(xmin, ymin), cellwidth, cellheight, nodata)
-	# arcpy.Define_projection(heightBinaryRas, sr)
 	outBinaryRas.save(outBinary)
-	# arcpy.Define_projection(outBinary, sr)
 
+	# Create a shapefile as well
+	# For the next version, remove the polygon for the label id 0
 	arcpy.AddMessage(outFc)
 	arcpy.RasterToPolygon_conversion(outBinary, outFc, "SIMPLIFY", "VALUE")
 
